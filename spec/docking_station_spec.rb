a33ee306fca561b37852f7094e772e6b7521d656
Ajitsy <sanghera.p@gmail.com>
2015-08-24 15:50:20 +0100

    using doubles inside docking_station_spec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,9 +4,8 @@ describe DockingStation do
   it{ is_expected.to respond_to :release_bike }
 
   it 'releases working bikes' do
-    subject.dock Bike.new
-    bike = subject.release_bike
-    expect(bike).to be_working
+    subject.dock double :bike, working?: true
+    expect(subject.release_bike).to be_working
   end
 
   it 'has a default capacity' do
@@ -17,26 +16,19 @@ describe DockingStation do
     subject.capacity = 10
     expect(subject.capacity).to eq 10
   end
-  
+
   it{ is_expected.to respond_to(:dock).with(1).argument }
 
   describe '#release_bike' do
     it 'raises an error when there are no bikes available' do
       expect { subject.release_bike }.to raise_error 'No bikes available'
     end
-
-    it 'not to release broken bike' do
-      bike = Bike.new
-      subject.dock bike
-      bike.report_broken
-      expect { subject.release_bike }.to raise_error 'No bikes available'
-    end
   end
 
   describe '#dock' do
     it 'raises an error when full' do
-      subject.capacity.times { subject.dock Bike.new}
-      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+      subject.capacity.times { subject.dock double :bike}
+      expect { subject.dock double :bike }.to raise_error 'Docking station full'
     end
   end
 end
